0cacb87d5f4062abbb6fdc9b63c3d51ec63090a8
Haowen Zhang <hwzhang@gatech.edu>
2017-11-12 15:32:34 -0500

    direct output to files

--- src/approx.cpp
+++ src/approx.cpp
@@ -1,9 +1,18 @@
 #include "approx.hpp"
-#include <stack>
 
 using namespace std;
 
-int approx(Graph &graph){
+double get_time(){
+    struct timeval time;  
+    double cur_time;  
+    gettimeofday(&time,NULL);  
+    cur_time=1000000*(time.tv_sec)+time.tv_usec;  
+    cur_time/=1000000;
+    return cur_time;
+}
+
+int approx(Graph &graph, string &instName, double cutoff){
+    double start_time = get_time();
     vector <int> solution;
     solution.reserve(graph.sizeV);
     vector <bool> visited(graph.sizeV+1);
@@ -28,6 +37,7 @@ int approx(Graph &graph){
             }
         }
     }
+    double real_time = get_time() - start_time;
     cout<<endl;
     cout<< solution.size()<<endl;
     for(const auto it: solution){
@@ -37,5 +47,32 @@ int approx(Graph &graph){
     if(graph.isVC(solution)){
         cout<<"yes!"<<endl;
     }
+
+    ofstream solution_file;
+    ostringstream string_stream;
+    string_stream << instName << "_Approx_" << cutoff << ".sol";
+    string solution_file_name;
+    solution_file_name = string_stream.str();
+    solution_file.open(solution_file_name);
+    solution_file<< solution.size()<<endl;
+    for(const auto it: solution){
+        solution_file << it ;
+        if(it != solution.back())
+            solution_file << ",";
+    }
+    solution_file << endl;
+    solution_file.close();
+    
+    string_stream.clear();
+    string_stream.str(string());
+    string_stream << instName << "_Approx_" << cutoff <<".trace";
+    ofstream trace_file;
+    string trace_file_name = string_stream.str();
+    trace_file.open(trace_file_name);
+    trace_file << real_time << ","; 
+    trace_file << solution.size() << endl;
+    trace_file.close();
+
+
     return 0;
 }
