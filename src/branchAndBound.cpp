e360c75b2da79bdab5d67104462d2ab70f21a3e7
William Kong <wwkong92@gmail.com>
2017-11-11 19:51:24 -0500

    Made adjacency list public; corrected timing

--- src/branchAndBound.cpp
+++ src/branchAndBound.cpp
@@ -51,8 +51,8 @@ void branchAndBoundIter(BnBInfo *B, GRBModel *M) {
   diffSec = ((sEnd + endTime.tv_nsec/1000000.0) - (sStart + startTime.tv_nsec/1000000.0))/1000;
   clock_gettime(CLOCK_REALTIME, &startTime);
   B->time += diffSec;
-  if (B->time >= B->cutoff*60) {
-    B->time = B->cutoff*60;
+  if (B->time >= B->cutoff) {
+    B->time = B->cutoff;
     return;
   }
 
@@ -90,8 +90,8 @@ void branchAndBoundIter(BnBInfo *B, GRBModel *M) {
   diffSec = ((sEnd + endTime.tv_nsec/1000000.0) - (sStart + startTime.tv_nsec/1000000.0))/1000;
   clock_gettime(CLOCK_REALTIME, &startTime);
   B->time += diffSec;
-  if (B->time >= B->cutoff*60) {
-    B->time = B->cutoff*60;
+  if (B->time >= B->cutoff) {
+    B->time = B->cutoff;
     return;
   }
 
@@ -115,8 +115,8 @@ void branchAndBoundIter(BnBInfo *B, GRBModel *M) {
   }
 
   // Check our runtime
-  if (B->time >= B->cutoff*60) {
-    B->time = B->cutoff*60;
+  if (B->time >= B->cutoff) {
+    B->time = B->cutoff;
     return;
   }
 
@@ -147,8 +147,8 @@ void branchAndBoundIter(BnBInfo *B, GRBModel *M) {
   diffSec = ((sEnd + endTime.tv_nsec/1000000.0) - (sStart + startTime.tv_nsec/1000000.0))/1000;
   clock_gettime(CLOCK_REALTIME, &startTime);
   B->time += diffSec;
-  if (B->time >= B->cutoff*60) {
-    B->time = B->cutoff*60;
+  if (B->time >= B->cutoff) {
+    B->time = B->cutoff;
     return;
   }
 
@@ -173,8 +173,8 @@ void branchAndBoundIter(BnBInfo *B, GRBModel *M) {
   }
 
   // Check our runtime
-  if (B->time >= B->cutoff*60) {
-    B->time = B->cutoff*60;
+  if (B->time >= B->cutoff) {
+    B->time = B->cutoff;
     return;
   }
 
